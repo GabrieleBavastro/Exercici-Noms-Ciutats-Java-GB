b3b4d05f974ce0ed2c4e984049406425d0a3b2b7
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-18 11:21:37 +0100

    Update Fase2.

--- Fase2.java
+++ Fase2.java
@@ -1,26 +1,15 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class Fase2 {
-	  public static void main(String[] args) {
-			Scanner scan = new Scanner(System.in);
-			 
-			//Decide the number of cities
-			System.out.print("Enter how many cities you want to write: ");
-			int numOfCities = Integer.parseInt(scan.nextLine());
-	 
-			//Create a string array to store the names of the cities
-			String arrayOfCities[] = new String[numOfCities];
-			for (int i = 0; i < arrayOfCities.length; i++) {
-				System.out.print("Enter the name of the city " + (i+1) + " : ");
-			        arrayOfCities[i] = scan.nextLine();
-			}
-			    
-			//Now show your cities's name one by one in alphabetic order
-			Arrays.sort(arrayOfCities);
-			for (int i = 0; i < arrayOfCities.length; i++) {
-				System.out.print("My cities in alphabetic order are : ");
-			        System.out.print(arrayOfCities[i] + "\n");
-			}
-			scan.close();
-  }
+  public static void main(String[] args) {
+	  
+	String [] arrayCiutat = new String [6];
+	for (int i=0;i<6;i++){
+	arrayCiutat[i]=JOptionPane.showInputDialog("Introduce the name of the city " + (i+1) + " and press enter.");
+		}
+	Arrays.sort(arrayCiutat);
+	for (int i=0; i<arrayCiutat.length;i++){
+	System.out.println("City " + (i+1) + ": " + arrayCiutat [i]);}
+
+	}
 }
